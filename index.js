f0769ed8acdb63aa5c60fa64d6e4caeab1fa3a55
alexsandrbarabash <sbarabas176@gmail.com>
2020-12-08 20:04:11 +0200

    Add router for player

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use( bodyParser.json() );
+//routers
+const player = require("./routers/player");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() => {
-    console.log(`Server start on port ${PORT}`);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/player", database);
+
+app.listen(PORT, () => {
+  console.log(`Server start on port ${PORT}`);
 });
