ac4f07fa2be5edb1f2a712fa8eff0593ced36f07
alexsandrbarabash <sbarabas176@gmail.com>
2020-12-11 03:03:59 +0200

    Add player back

---
+++ host.js
@@ -0,0 +1,4 @@
+const host = 'http://192.168.0.5:3000';
+module.exports = {
+    host
+}
--- routers/player.js
+++ routers/player.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
+const {host} = require('../host');
 
 router.post("/audio", async (req, res) => {
   let book = {};
@@ -17,10 +18,8 @@ router.post("/audio", async (req, res) => {
 
   await db.serialize(() => {
     db.get(
-      `SELECT b.id, b.title, b.artwork, p.size_query FROM books b INNER JOIN
-    (SELECT book_id, COUNT(book_id) AS size_query FROM part GROUP BY book_id) p
-     ON b.id=p.book_id  WHERE id=?;`,
-      [req.body.id],
+      `SELECT b.id, b.title, (? || b.artwork) AS artwork FROM books b WHERE id=?;`,
+      [host, req.body.id],
       (err, row) => {
         if (err) {
           return console.log(err.message);
@@ -30,12 +29,12 @@ router.post("/audio", async (req, res) => {
     );
 
     db.all(
-      `SELECT p.numbering AS id, p.book_audio AS url, b.title, b.artwork, a.name AS artist FROM part p
+      `SELECT p.numbering AS id, ( ? || p.book_audio) AS url, b.title, ( ? || b.artwork) AS artwork, a.name AS artist FROM part p
     INNER JOIN books b ON b.id=p.book_id
-    INNER JOIN (SELECT book_id, autor_id FROM autor_book WHERE book_id=1 LIMIT 1) ab ON p.book_id = ab.book_id
+    INNER JOIN (SELECT book_id, autor_id FROM autor_book WHERE book_id=? LIMIT 1) ab ON p.book_id = ab.book_id
     INNER JOIN autors a ON a.id=ab.autor_id
-    WHERE p.book_id=?;`,
-      [req.body.id],
+    WHERE p.book_id=? ORDER BY p.numbering;`,
+      [host, host,req.body.id, req.body.id],
       (err, row) => {
         if (err) {
           return console.log(err.message);
