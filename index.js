4c96341fe671e51cef920a0277dcde333b00bc97
alexsandrbarabash <sbarabas176@gmail.com>
2020-12-18 03:52:43 +0200

    something

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
 const PORT = process.env.PORT || 3000;
+
+app.use(fileUpload({ createParentPath: true }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
